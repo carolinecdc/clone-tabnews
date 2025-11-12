7c42542af540d5ebaa4f78626b3276040cfffdb9
Caroline Duarte Costa <141359236+carolinecdc@users.noreply.github.com>
2025-11-12 11:54:02 +0000

    change the phrase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home(){
     return <div>
-        <h1>Hello Gorgeous 😍</h1></div>
+        <h1> 😍 Hello Gorgeous 😍</h1>
+        <p>Bacon ipsum dolor amet pork cupim tri-tip bresaola short ribs chuck. Andouille shankle kielbasa pork loin pork belly pig cow alcatra fatback burgdoggen t-bone ball tip biltong. Tail shoulder andouille pork chop boudin, spare ribs turkey buffalo bresaola. Pastrami andouille pork belly landjaeger kevin ribeye swine capicola, doner fatback turkey meatball rump chuck. Sirloin chuck tri-tip jowl buffalo, turkey fatback meatloaf strip steak venison shoulder capicola.</p>
+        </div>
 }
 
 export default Home;
