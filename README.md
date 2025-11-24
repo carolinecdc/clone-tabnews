86fb7aaa34bfeea6b317588abaa96dd48bdbf604
Caroline Duarte Costa <141359236+carolinecdc@users.noreply.github.com>
2025-11-24 16:05:00 +0000

    scripts add  and

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Tabnews implementation /curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
-function Home(){
-    return <div>
-        <h1> 😍 Hello Gorgeous 😍</h1>
-        <p>a short ribs chuck. Andouille shankle kielbasa.</p>
-        </div>
+function Home() {
+  return (
+    <div>
+      <h1> 😍 Hello Gorgeous 😍</h1>
+      <p>a short ribs chuck. Andouille shankle kielbasa.</p>
+    </div>
+  );
 }
 
 export default Home;
